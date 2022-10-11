79fa661e95dc54e774079e03fd474069ceba3f14
Herlesio <114457312+Herlesio@users.noreply.github.com>
2022-10-11 16:41:01 -0600

    Update Hotel Revenue..sql

--- Hotel Revenue..sql
+++ Hotel Revenue..sql
@@ -12,7 +12,9 @@ select * from dbo.['2020$']
 -- Is the Hotel revenue growing?
 -- A) Finding the total revenue amount for each year and for each hotel type.
 -- Revenues were obtained by adding up all nights spent in hotels during the week and weekends and multiplying them by the daily rate.
--- We can definitely conclude that revenues grew between 2018 and 2020.
+	--Answer is in 2018 the revenue was 4,885,517 dollars.
+	--In 2019 the revenue was 20,188,409 dollars.
+	--In 2020 the revenue was 14,284,246 dollars. We can conlude that the revenue is increasing annually despite the drop in 2020 due to Covid-19 restrictions. 
 
 with Hotels as (
 	select * from dbo.['2018$']
